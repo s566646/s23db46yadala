43903dd037fbc983ad21d0c9d98d1a4028d7f651
s566646 <143014647+s566646@users.noreply.github.com>
2023-11-15 19:20:02 -0600

    sc7

--- routes/student.js
+++ routes/student.js
@@ -4,6 +4,6 @@ var router = express.Router();
 /* GET costumes */
 router.get('/', Student_controlers.Student_view_all_Page );
 
-
+router.get('/detail', Student_controlers.Student_view_one_Page);
 
 module.exports = router;
